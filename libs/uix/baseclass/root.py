fd9fa669a811c86ed431a78d0636ebc0bd3f1400
Sandeep Jadam <withsj.in@gmail.com>
2022-08-17 20:28:42 +0530

    previous screen root update

--- libs/uix/baseclass/root.py
+++ libs/uix/baseclass/root.py
@@ -1,6 +1,8 @@
 from kivy.core.window import Window
 
 from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 from libs.applibs import utils
 
 utils.load_kv("root.kv")
@@ -10,6 +12,7 @@ class Root(ScreenManager):
         super(Root, self).__init__(**kwargs)
         Window.bind(on_keyboard=self._key_handler)
         self.screen_list = list() #this list have all screen that user switched
+        self.dialog = None
     
     def _key_handler(self, instance, key, *args):
         
@@ -17,17 +20,42 @@ class Root(ScreenManager):
             #in Desktop this key 27 is Esc and in Phone it's Back btn
             self.previous_screen()
             return True
-    
+    def dialog_close(self, *args):
+        self.dialog.dismiss(force=True)
+        self.screen_list.append(self.last_screen)
     def previous_screen(self):
         """
         Switch to previous screen last screen in screen_list
         """
-        last_screen=self.screen_list.pop()
-        if last_screen == "home" or last_screen == "login":
-            exit()
-        print(self.screen_list)
-        self.transition.direction = "left"
-        self.current = self.screen_list[len(self.screen_list)-1]
+        if len(self.screen_list) > 1 : 
+            self.last_screen=self.screen_list.pop()
+        else:
+            self.last_screen = "login"
+
+        if self.last_screen == "home" or self.last_screen == "login":
+            if not self.dialog:
+                self.dialog = MDDialog(
+                    text="Do you want to exit?",
+                    buttons=[
+                        MDFlatButton(
+                            text="CANCEL",
+                            on_release= self.dialog_close
+                        ),
+                        MDFlatButton(
+                            text="Exit",
+                            on_release= exit
+                        ),
+                    ],
+                )
+            
+            self.dialog.open()
+        
+        else:
+            print(self.screen_list)
+            self.transition.direction = "left"
+            self.current = self.screen_list[len(self.screen_list)-1]
+            
+        
         
         
     
