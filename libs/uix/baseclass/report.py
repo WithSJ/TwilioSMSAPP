9877122c6c1f7041fd9cf633c91ab72d103f2a65
Sandeep Jadam <withsj.in@gmail.com>
2022-08-17 19:57:42 +0530

    data table report ui

--- libs/uix/baseclass/report.py
+++ libs/uix/baseclass/report.py
@@ -2,12 +2,97 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.list import MDList
+from kivymd.uix.datatables import MDDataTable
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.metrics import dp
 from libs.applibs import utils
 
 utils.load_kv("report.kv")
 
 class Report_Screen(MDScreen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.data_tables = MDDataTable(
+            size_hint=(0.8, 0.8),
+            pos_hint= {"center_x":0.5, "center_y":0.5},
+            use_pagination=True,
+            # name column, width column, sorting function column(optional)
+            column_data=[
+                ("No.", dp(30)),
+                ("Status", dp(30)),
+                ("Phone Number", dp(60)),
+                ("Severity", dp(30)),
+                ("Stage", dp(30)),
+                ("Schedule", dp(30), lambda *args: print("Sorted using Schedule")),
+                ("Team Lead", dp(30)),
+            ],
+            row_data=[
+                # The number of elements must match the length
+                # of the `column_data` list.
+                (
+                    "1",
+                    ("alert", [255 / 256, 165 / 256, 0, 1], "No Signal"),
+                    "Astrid: NE shared managed",
+                    "Medium",
+                    "Triaged",
+                    "0:33",
+                    "Chase Nguyen",
+                ),
+                (
+                    "2",
+                    ("alert-circle", [1, 0, 0, 1], "Offline"),
+                    "Cosmo: prod shared ares",
+                    "Huge",
+                    "Triaged",
+                    "0:39",
+                    "Brie Furman",
+                ),
+                (
+                    "3",
+                    (
+                        "checkbox-marked-circle",
+                        [39 / 256, 174 / 256, 96 / 256, 1],
+                        "Online",
+                    ),
+                    "Phoenix: prod shared lyra-lists",
+                    "Minor",
+                    "Not Triaged",
+                    "3:12",
+                    "Jeremy lake",
+                ),
+                (
+                    "4",
+                    (
+                        "checkbox-marked-circle",
+                        [39 / 256, 174 / 256, 96 / 256, 1],
+                        "Online",
+                    ),
+                    "Sirius: NW prod shared locations",
+                    "Negligible",
+                    "Triaged",
+                    "13:18",
+                    "Angelica Howards",
+                ),
+                (
+                    "5",
+                    (
+                        "checkbox-marked-circle",
+                        [39 / 256, 174 / 256, 96 / 256, 1],
+                        "Online",
+                    ),
+                    "Sirius: prod independent account",
+                    "Negligible",
+                    "Triaged",
+                    "22:06",
+                    "Diane Okuma",
+                ),
+            ]
+        )
+        self.ids.data_table.add_widget(self.data_tables)
+
+        
+
 class ContentNavigationDrawer(MDBoxLayout):
     pass
 class DrawerList(ThemableBehavior, MDList):
