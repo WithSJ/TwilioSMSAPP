30c16b8e0e114673418679e91911679c397b0de3
Sandeep Jadam <withsj.in@gmail.com>
2022-08-18 20:49:19 +0530

    authentication with GUI intregated

--- libs/uix/baseclass/login.py
+++ libs/uix/baseclass/login.py
@@ -1,7 +1,11 @@
 from kivymd.uix.screen import MDScreen
-from libs.applibs import utils
+from kivymd.uix.snackbar import Snackbar
+from libs.applibs import utils,authentication
 
 utils.load_kv("login.kv")
 
 class Login_Screen(MDScreen):
-    pass
+    def app_login(self,username,password):
+        msg= authentication.login(username.text,password.text)
+        Snackbar(text= msg[1]).open()
+
--- libs/uix/baseclass/signup.py
+++ libs/uix/baseclass/signup.py
@@ -1,7 +1,13 @@
 from kivymd.uix.screen import MDScreen
-from libs.applibs import utils
+from kivymd.uix.snackbar import Snackbar
+from libs.applibs import utils,authentication
 
 utils.load_kv("signup.kv")
 
 class Signup_Screen(MDScreen):
-    pass
+    def app_signup(self,username,password,confirm_password):
+        if password.text != confirm_password.text:
+            Snackbar(text= "Password not match...").open()
+        else:
+            msg= authentication.signup(username.text,password.text)
+            Snackbar(text= msg[1]).open()
