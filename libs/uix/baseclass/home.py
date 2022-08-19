b3ce592ac2440f6fbb0e3611511f5e368e5a2ff7
Sandeep Jadam <withsj.in@gmail.com>
2022-08-19 19:22:56 +0530

    update file chooser

--- libs/uix/baseclass/home.py
+++ libs/uix/baseclass/home.py
@@ -1,45 +1,38 @@
-from ast import Pass
-from kivymd.uix.list import  ImageLeftWidget
 from kivymd.uix.screen import MDScreen 
-from kivymd.uix.list import TwoLineAvatarListItem
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.list import MDList
-from kivymd.uix.filemanager import MDFileManager
 
 from libs.applibs import utils
-
+from plyer import filechooser
 utils.load_kv("home.kv")
 
 class Home_Screen(MDScreen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        #Window.bind(on_keyboard=self.events)
-        self.manager_open = False
-        self.file_manager = MDFileManager(
-            exit_manager=self.exit_manager,
-            select_path=self.select_path,
-            md_bg_color= "#e7e4c0")
-    
+
     def file_manager_open(self):
-        self.file_manager.show('/')  # output manager to the screen
-        self.manager_open = True
-    
+        filechooser.open_file(on_selection=self.select_path,multiple=True)
+
     def select_path(self, path):
         '''It will be called when you click on the file name
         or the catalog selection button.
 
-        :type path: str;
+        :type path: str;4
         :param path: path to the selected directory or file;
         '''
-        self.exit_manager()
-        self.ids.filenamefield.text = path
-    
-    def exit_manager(self, *args):
-        '''Called when the user reaches the root of the directory tree.'''
+        self.path = path
+        self.ids.filenamefield.text = ", ".join(path)
 
-        self.manager_open = False
-        self.file_manager.close()
+    def send_to_all(self):
+        """Send sms to all phone numbers"""
+        print("Send to all clicked")
+        for selected_file in self.path:
+            with open(selected_file) as filedata:
+                for number in filedata.readlines():
+                    print(str(number).removesuffix("\n"))
+
+
+    
+    
 class ContentNavigationDrawer(MDBoxLayout):
     pass
 class DrawerList(ThemableBehavior, MDList):
